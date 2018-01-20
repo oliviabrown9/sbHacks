e2dfe3e07ffc045be0be7122e10652e915930d80
Olivia <obrown917@gmail.com>
2018-01-20 02:56:39 -0800

    Firebase store image

--- sbhacks/ViewController.swift
+++ sbhacks/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+    
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        ref = Database.database().reference()
+        
+        // take photo
+        
+        let image: UIImage?
+        
+        
+        
         Auth.auth().signInAnonymously() { (user, error) in
+            var data = Data()
+            data = UIImageJPEGRepresentation(image!, 0.8)!
+            // set upload path
+            let filePath = "\(user!.uid)/\("userPhoto")"
+            let metaData = FIRStorageMetadata()
+            metaData.contentType = "image/jpg"
+            self.storageRef.child(filePath).putData(data, metadata: metaData){(metaData,error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+                else {
+                    //store downloadURL
+                    let downloadURL = metaData!.downloadURL()!.absoluteString
+                    //store downloadURL at database
+                    self.databaseRef.child("Users").child(user!.uid).updateChildValues(["photo": downloadURL])
+                }
+            }
         }
     }
 }
